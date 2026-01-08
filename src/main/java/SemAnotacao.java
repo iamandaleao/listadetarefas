1e28b5efc60dd2b5b7cfefe5a723bb726c35d22e
iamandaleao <amandabibiano144@gmail.com>
2026-01-08 19:29:53 -0300

    refactor: rewrite code without annotations

---
+++ src/main/java/SemAnotacao.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListaDeTarefas {
+  public static void main(String[] args) {
+    ArrayList<String> tarefas = new ArrayList<>();
+
+  }
+}
