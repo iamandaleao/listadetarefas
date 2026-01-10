e35924794fe250fb1b059beb2a0fbf1854b92729
iamandaleao <amandabibiano144@gmail.com>
2026-01-09 22:16:22 -0300

    refactor: rewrite code wihout annotations

--- src/main/java/SemAnotacao.java
+++ src/main/java/SemAnotacao.java
@@ -5,7 +5,7 @@ public class ListaDeTarefas {
   public static void main(String[] args) {
     ArrayList<String> tarefas = new ArrayList<>(); 
     ArrayList<Boolean> concluidas = new ArrayList<>();
-
+    
 
 
   }
